4ca75939c15c011a945fd40988025150cfa47939
wtcarrin <wtcarrin@go.olemiss.edu>
2025-03-25 22:28:40 -0500

    fix?

--- app/viewSingleOrder/page.tsx
+++ app/viewSingleOrder/page.tsx
@@ -1,26 +1,12 @@
 import { createClient } from '@/utils/supabase/server';
 import { revalidatePath, revalidateTag } from 'next/cache';
 import { redirect  } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
-export default async function Orders() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">...</h1>
-      <h4>This is the page for viewing a single order, having some trouble with using searchparams in the url atm</h4>
-    </div>
-  );
-}
-
-/*import { createClient } from '@/utils/supabase/server';
-
-export default async function ViewSingleOrder({
-  params,
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const orderId = searchParams.q as string;
+export default async function ViewSingleOrder() 
+{
+  const searchParams = useSearchParams()
+  const orderId = searchParams.get('q') as string;
   const supabase = await createClient()
 
   if (!orderId) {
@@ -46,4 +32,4 @@ export default async function ViewSingleOrder({
     </div>
   );
 }
-  */
+  
