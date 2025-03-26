82b72d34e8c5bf9f317390f9ae38b1d2fabacc0d
wtcarrin <wtcarrin@go.olemiss.edu>
2025-03-25 21:07:59 -0500

    again

--- app/viewSingleOrder/page.tsx
+++ app/viewSingleOrder/page.tsx
@@ -1,3 +1,16 @@
+import { createClient } from '@/utils/supabase/server';
+import { revalidatePath, revalidateTag } from 'next/cache';
+import { redirect  } from 'next/navigation';
+
+export default async function Orders() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">...</h1>
+      <h4>This is the page for viewing a single order, having some trouble with using searchparams in the url atm</h4>
+    </div>
+  );
+}
+
 /*import { createClient } from '@/utils/supabase/server';
 
 export default async function ViewSingleOrder({
